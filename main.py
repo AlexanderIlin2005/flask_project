563d71eab9a94ca4a273cd2750de335cdef758a2
AlexanderIlin2005 <sashailyin2005@gmail.com>
2021-04-16 18:07:00 +0300

    login
    Добавлена заполненная контентом база. login.html, base.html. Добавлены обработчики index и login.
    
    Co-Authored-By: drakanoy <75383693+drakanoy@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask
-from data import db_session
+from flask import Flask, render_template, redirect, request, make_response
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+from forms.user import RegisterForm, LoginForm
 from data.users import User
+from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 # просто коментарий
+
+
+app.route("/index")
+app.route("/")
+def index():
+    return render_template("templates/index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
 
 
 def main():
